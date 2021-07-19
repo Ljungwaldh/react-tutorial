c45ce4ed2e1024c9ba42b8df4493a91300c6498f
Gustaf Ljungwaldh <gustaf.ljungwaldh@live.co.uk>
2021-07-19 06:16:25 +0200

    Initial tutorial code done, working on js code

---
+++ my-app/src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Square extends React.Component {
+    render() {
+        return (
+            <button className="square">
+                {/* TODO */}
+            </button>
+        );
+    }
+}
+
+class Board extends React.Component {
+    renderSquare(i) {
+        return <Square />;
+    }
+
+    render() {
+        const status = 'Next player: X';
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                
+            </div>
+        )
+    }
+}
